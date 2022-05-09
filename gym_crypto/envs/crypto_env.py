7a3a0b35c656e81960573fa6b59f10d7951e4fd9
Joren-vanGoethem <jorenvangoethem@hotmail.com>
2022-05-09 12:27:36 +0200

    finish buy and hold rewards function

--- gym_crypto/envs/crypto_env.py
+++ gym_crypto/envs/crypto_env.py
@@ -10,8 +10,8 @@ class CryptoEnv(TradingEnv):
         self.frame_bound = frame_bound
         super().__init__(df, window_size)
 
-        self.trade_fee_bid_percent = 0.01  # percentage of value
-        self.trade_fee_ask_percent = 0.01  # percentage of value
+        self.trade_fee_bid_percent = 0.01  # percentage
+        self.trade_fee_ask_percent = 0.01  # percentage
 
         self._tick_range = 10
         self._reward_bonus = 1
@@ -43,133 +43,46 @@ class CryptoEnv(TradingEnv):
         elif action == Actions.HOLD.value:
             step_reward = self.hold_reward()
 
-        # current_price = self.prices[self._current_tick]
-        # # last_trade_price = self.prices[self._last_trade_tick]
-        # last_buy_price = self.prices[self._last_buy_tick]
-        # last_sell_price = self.prices[self._last_sell_tick]
-
-        # if action == Actions.BUY.value and self._position == Positions.NO:
-        #     # BUY
-        #     # check previous values if trend is downward up to buy point
-        #     p_lowest = current_price
-        #     imperfect = False
-            
-        #     for tick in range(self._current_tick-self._tick_range, self._current_tick):
-        #         curr_price = self.prices[tick]
-
-        #         if curr_price < current_price:
-        #             #bad
-        #             imperfect = True
-        #             if curr_price < p_lowest:
-        #                 p_lowest = curr_price
-
-        #         elif curr_price > current_price:
-        #             #good
-        #             step_reward += self._reward_bonus
-                
-        #     if imperfect:
-        #         step_reward -= ((current_price - p_lowest) / p_lowest) * 100
-
-        #     p_lowest = current_price
-        #     imperfect = False
-
-        #     # check next values if trend is upward
-        #     for tick in range(self._current_tick, self._current_tick+self._tick_range):
-        #         curr_price = self.prices[tick]
-
-        #         if curr_price < current_price:
-        #             #bad
-        #             imperfect = True
-        #             if curr_price < p_lowest:
-        #                 p_lowest = curr_price
-
-        #         elif curr_price > current_price:
-        #             #good
-        #             step_reward += self._reward_bonus
-
-        #     if imperfect:
-        #         step_reward -= ((current_price - p_lowest) / p_lowest) * 100
-
-        # elif action == Actions.SELL.value and self._position == Positions.YES:
-        #     # SELL
-        #     # check previous values if trend is upward
-        #     p_highest = current_price
-        #     test_before = False
-        #     test_after = False
-            
-        #     for tick in range(self._last_buy_tick, self._current_tick):
-        #         curr_price = self.prices[tick]
-
-        #         if curr_price < current_price:
-        #             #good
-        #             step_reward += self._reward_bonus
-
-        #         elif curr_price > current_price:
-        #             #bad
-        #             test_before = True
-        #             if curr_price > p_highest:
-        #                 p_highest = curr_price
-                
-        #     # if imperfect:
-        #     #     step_reward -= ((p_highest - current_price) / current_price) * 100
-
-        #     p_highest = current_price
-
-        #     # check next values if trend is downward
-        #     for tick in range(self._current_tick, self._current_tick+self._tick_range):
-        #         curr_price = self.prices[tick]
-
-        #         if curr_price < current_price:
-        #             #good
-        #             step_reward += self._reward_bonus
-
-        #         elif curr_price > current_price:
-        #             #bad
-        #             test_after = True
-        #             if curr_price > p_highest:
-        #                 p_highest = curr_price
-
-        #     if test_before and test_after:
-        #         step_reward -= ((p_highest - current_price) / current_price) * 100
-            
-        #     step_reward += (last_buy_price / current_price) / current_price
-       
-        # elif action == Actions.HOLD.value:
-        #     # HOLD
-        #     # check if value is higher than last buy
-        #     if self._position == Positions.YES:
-        #         if current_price >= last_buy_price:
-        #             step_reward += self._reward_bonus
-        #         else:
-        #             step_reward -= self._reward_bonus
-            
-        #     # check next values if trend is upward for at least 1 candle
-        #     if self.prices[self._current_tick+1] >= current_price:
-        #         step_reward += self._reward_bonus
-        #     else:
-        #         step_reward -= self._reward_bonus
-
-        # elif action == Actions.BUY.value and self._position == Positions.YES:
-        #     # BUY when position was True or SELL when position was False
-        #     if current_price < last_buy_price:
-        #         reward += self._reward_bonus/10
-        #     else:
-        #         reward -= self._reward_bonus
-        
-        # elif action == Actions.SELL.value and self._position == Positions.NO:
-            # BUY when position was True or SELL when position was False
-            if current_price > last_sell_price:
-                reward += self._reward_bonus/10
-            else:
-                reward -= self._reward_bonus
-
-
-            pass
-
         return step_reward
     
     def buy_reward(self):
-        pass
+        step_reward = 0
+
+        current_price = self.prices[self._current_tick]
+        # last_trade_price = self.prices[self._last_trade_tick]
+        last_buy_price = self.prices[self._last_buy_tick]
+        last_sell_price = self.prices[self._last_sell_tick]
+
+        if self._position == Positions.YES:
+            temp_max_price = current_price
+            temp_min_price = current_price
+
+            for tick in range(self._last_sell_tick, self._current_tick + self._look_ahead_range):
+                price = self.prices[tick]
+
+                if price > current_price:
+                    #good
+                    if price > temp_max_price:
+                        temp_max_price = price
+
+                if price < current_price:
+                    #bad, means this buy is sub-optimal
+                    if price < temp_min_price:
+                        temp_min_price = price
+                
+            if current_price > temp_min_price:
+                #punishment for buying too high
+                step_reward += ( (temp_min_price - current_price) / current_price ) * 100
+
+            if current_price < temp_max_price:
+                #reward for buying at low point
+                step_reward += ( (temp_max_price - current_price) / current_price ) * 100
+
+        else:
+            #will be negative if this price is higher than the previous price and positive if reverse
+            step_reward += ( (last_buy_price - current_price) / current_price ) * 100
+
+        return step_reward
 
     def sell_reward(self):
         step_reward = 0
@@ -207,40 +120,44 @@ class CryptoEnv(TradingEnv):
                 #punishment for selling too low
                 step_reward += ( (current_price - temp_max_price) / temp_max_price ) * 100
 
-            else:
-                #reward for selling at highest point
+            if current_price > temp_min_price:
+                #reward for selling above lowest point
                 step_reward += ( (current_price - temp_min_price) / temp_min_price ) * 100
             
         else:
             #scenario 1
             #Sell after another sell that was at a higher price, bad sell
-            if current_price > last_sell_price:
-                step_reward += ( (current_price - last_sell_price) / last_sell_price ) * 100
-            
             #scenario 2
             #Sell after another sell that was at a lower price, good sell 
-            if current_price > last_sell_price:
-                step_reward += ( (current_price - last_sell_price) / last_sell_price ) * 100
+            step_reward += ( (current_price - last_sell_price) / last_sell_price ) * 100
 
         return step_reward
         
-
     def hold_reward(self):
-        pass
+        step_reward = 0
+
+        current_price = self.prices[self._current_tick]
+        previous_price = self.prices[self._current_tick-1]
+        next_price = self.prices[self._current_tick+1]
+
+        if self._position == Positions.YES and previous_price < current_price and next_price > current_price:
+            step_reward += ( ( next_price - previous_price ) / previous_price ) * 100
+        elif previous_price > current_price and next_price < current_price:
+            step_reward += ( ( previous_price - next_price ) / next_price ) * 100
+
+        return step_reward
 
     def _update_profit(self, action):
-        trade = False
-        if (action == Actions.BUY.value and self._position == Positions.NO) or (
-            action == Actions.SELL.value and self._position == Positions.YES
-        ):
-            trade = True
+        sell = False
+        if action == Actions.SELL.value and self._position == Positions.YES:
+            sell = True
 
-        if trade or self._done:
+        if sell or self._done:
             current_price = self.prices[self._current_tick]
-            last_trade_price = self.prices[self._last_trade_tick]
+            last_buy_price = self.prices[self._last_buy_tick]
 
             if self._position == Positions.YES:
-                shares = (self._total_profit * (1 - self.trade_fee_ask_percent)) / last_trade_price
+                shares = (self._total_profit * (1 - self.trade_fee_ask_percent)) / last_buy_price
                 self._total_profit = (shares * (1 - self.trade_fee_bid_percent)) * current_price
 
     def max_possible_profit(self):
